e58304be8eadc9bf9fd4c473fb3d15a539229529
tintb03 <128471718+tintb03@users.noreply.github.com>
2023-08-12 10:23:23 +0700

    FrontEnd DashBoard part 1

--- resources/views/Admin/main.blade.php
+++ resources/views/Admin/main.blade.php
@@ -1,226 +1,225 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="en" >
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Admin DashBoard</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
- 	<style type="text/css">
-        /* CSS cho thanh sidebar */
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: Arial, sans-serif;
-            background-color: #f1f1f1;
-        }
-        ul {
-            list-style-type: none;
-            padding-left: 1px; /* Điều chỉnh giá trị theo mong muốn */
-            padding-left: 0px;
- 
-        }
+  <meta charset="UTF-8">
+    <meta name="apple-mobile-web-app-title" content="CodePen">
+  <title>Admin Dashboard</title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css'>
 
-        li {
-            padding-left: 20px; /* Điều chỉnh giá trị theo mong muốn */
-        }
+<style>
+header,
+main,
+footer {
+  padding-left: 240px;
+}
 
+body {
+  background-color: white;
+}
 
-        .sidebar {
-            position: fixed;
-            top: 0;
-            left: 0;
-            bottom: 0;
-            width: 250px;
-            background: #FFFFFF;
-        }
+@media only screen and (max-width: 992px) {
+  header,
+  main,
+  footer {
+    padding-left: 0;
+  }
+}
 
-        .sidebar .top-bar {
-            background: #FFFFFF;
-            overflow: hidden;
-            text-align: center;
-        }
+#credits li,
+#credits li a {
+  color: white;
+}
 
-        .sidebar .top-bar .logo img {
-            width: 200px; /* Điều chỉnh kích thước theo ý muốn */
-            height: auto;
-        }
+#credits li a {
+  font-weight: bold;
+}
 
+.footer-copyright .container,
+.footer-copyright .container a {
+  color: #BCC2E2;
+}
 
-        /* ... (các phần CSS khác cho sidebar) ... */
+.fab-tip {
+  position: fixed;
+  right: 85px;
+  padding: 0px 0.5rem;
+  text-align: right;
+  background-color: #323232;
+  border-radius: 2px;
+  color: #FFF;
+  width: auto;
+}
+</style>
 
-        /* Định dạng cho phần tạo khoảng cách */
-        .separator {
-            border-top: 1px solid #111111;
-            border-bottom: 1px solid #353535;
-            margin: 20px;
-        }
+  <script>
+  window.console = window.console || function(t) {};
+</script>
 
-        /* Định dạng cho liên kết trong menu */
-        .menu{
-            padding-left: 20px;
-        }
-        .menu a {
-            
-            text-decoration: none;
-        }
-
-        .menu a:hover {
-            color: Red;
-        }
-
-        /* ... (các phần CSS khác cho menu) ... */
-
-        /* CSS cho phần nội dung chính */
-        .main {
-
-            margin-left: 250px; /* Khoảng cách với sidebar */
-        }
-
-        .navbar {
-            background: #FFFACD;
-            color: #FFF;
-            text-align: center;
-            margin: 0px;
-            height: 50px;
-        }
-
-        .top-bar {
-            background: #3AB0FF;
-            color: #FFF;
-            padding: 10px 0;
-            text-align: center;
-        }
-
-        .main-content {
-            background: #FFF;
-            padding: 20px;
-            border-radius: 5px;
-            height: 780px;
-            
-            /* background-image: url("https://btec.fpt.edu.vn/wp-content/uploads/2022/07/LogoBTEC-1536x1268.png");
-            background-size: cover;
-            background-repeat: no-repeat; */
-        }
-
-                /* Đặt kích thước ảnh theo tỷ lệ */
-        img {
-            width: 40%; /* Điều chỉnh kích thước chiều rộng dựa trên tỷ lệ phần trăm */
-            height: auto; /* Tự điều chỉnh chiều cao để duy trì tỷ lệ */
-            display: block;
-            margin: auto;
-        }
-
-
-        footer {
-            background-color: #333;
-            color: #fff;
-            text-align: center;
-            padding: 10px;
-            position: fixed;
-            bottom: 0;
-            width: 100%;
-        }
-
-        
-    </style>
+  
+  
 </head>
+
+<body translate="no">
+  <head>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
 <body>
-    <aside class="sidebar">
-        <div class="top-bar">
-            <p class="logo">
-                <a href="{{ route('admin.main') }}">
-                    <img src="https://th.bing.com/th/id/OIP.OSutm8Wgkof3WBByI7TIdAHaCT?pid=ImgDet&rs=1" alt="Logo">
-                </a>
-            </p>
+
+  <ul id="slide-out" class="side-nav fixed z-depth-2">
+    <li class="center no-padding">
+      <div class="indigo darken-2 white-text" style="height: 180px;">
+        <div class="row">
+        <img style="margin-top: 5%; border-radius: 50%;" width="100" height="100" src="https://play-lh.googleusercontent.com/vtF2gcADW6O7qnzipftCyGOyaB4pb12bjl4sMBcZp3KOOdf8DdHUJDVx0JeNeuT7nh3A"/>
+
+          <p style="margin-top: -13%;">
+            Tirth Patel
+          </p>
         </div>
+      </div>
+    </li>
 
-       
+   <li id="dash_dashboard"><a class="waves-effect" href="{{ route('admin.main') }}"><b>Dashboard</b></a></li>
 
-        <menu class="menu">
-                    <p class="menu-name">Quản Lý Tài Khoản</p>
-                    <ul>
-                                <li><a href="{{ route('admin.account.list') }}">View Account</a></li>
-                                <li><a href="{{ route('admin.account.create') }}">Create Account</a></li>
-                                <li><a href="{{ route('admin.account.changemyaccountpassword') }}">Change My Account Password</a></li>
+    <ul class="collapsible" data-collapsible="accordion">
+      <li id="dash_users">
+        <div id="dash_users_header" class="collapsible-header waves-effect"><b>Quản Lý Tài Khoản</b></div>
+        <div id="dash_users_body" class="collapsible-body">
+          <ul>
+            <li id="users_seller">
+              <a class="waves-effect" style="text-decoration: none;" href="{{ route('admin.account.list') }}">View Account</a>
+            </li>
+            <li id="users_seller">
+              <a class="waves-effect" style="text-decoration: none;" href="{{ route('admin.account.create') }}">Create Account</a>
+            </li>
+          </ul>
+        </div>
+      </li>
 
-                            </ul>
+      <li id="dash_products">
+        <div id="dash_products_header" class="collapsible-header waves-effect"><b>Quản Lý Chủ Khách Sạn</b></div>
+        <div id="dash_products_body" class="collapsible-body">
+          <ul>
+            <li id="products_product">
+              <a class="waves-effect" style="text-decoration: none;" href="{{ route('admin.hoteler.index') }}">View Hotelers</a>
+              <a class="waves-effect" style="text-decoration: none;" href="{{ route('admin.hoteler.create') }}">Create Hoteler</a>
+            </li>
+          </ul>
+        </div>
+      </li>
 
-                    <p class="menu-name">Quản Lý Chủ Khách Sạn</p>
-                            <ul>
-                                <li><a href="{{ route('admin.hoteler.index') }}">View Hotelers</a></li>
-                                <li><a href="{{ route('admin.hoteler.create') }}">Create Hoteler</a></li>
-                            </ul>
-                    <ul>
-                        
-                    <hr/>
-
-                    <p class="menu-name">Quản Lý Khách Sạn</p>
-                            <ul>
-                                <li><a href="#">View Hotels</a></li>
-                                <li><a href="#">Create Hotel</a></li>
-                            </ul>
-                    <ul>
-                        
-                    <p class="menu-name">Quản Lý Phòng</p>
-                            <ul>
-                                <li><a href="{{ route('admin.room.index') }}">View Rooms</a></li>
-                                <li><a href="{{ route('admin.room.create') }}">Create Room</a></li>
-                            </ul>
-                    <ul>
-
-                    <!-- <p class="menu-name">Quản Lý Courses</p>
-                            <ul>
-                                <li><a href="#">View Courses</a></li>
-                                <li><a href="#">Create Courses</a></li>
-
-                            </ul>
- -->
-
-        </menu>
-    </aside>
+      <li id="dash_products">
+        <div id="dash_products_header" class="collapsible-header waves-effect"><b>Quản Lý Phòng</b></div>
+        <div id="dash_products_body" class="collapsible-body">
+          <ul>
+            <li id="products_product">
+              <a class="waves-effect" style="text-decoration: none;" href="{{ route('admin.room.index') }}">View Rooms</a>
+              <a class="waves-effect" style="text-decoration: none;" href="{{ route('admin.room.create') }}">Create Rooms</a>
+            </li>
+          </ul>
+        </div>
+      </li>
 
 
 
-     <div class="main">
+      <li id="dash_brands">
+        <div id="dash_brands_header" class="collapsible-header waves-effect"><b>Brands</b></div>
+        <div id="dash_brands_body" class="collapsible-body">
+          <ul>
+            <li id="brands_brand">
+              <a class="waves-effect" style="text-decoration: none;" href="#!">Brand</a>
+            </li>
 
-     	   <div class="navbar">
-				<nav class="navbar navbar-inverse">
-				  <div class="container-fluid">
-				    <div class="navbar-header">
-				      <a class="navbar-brand" href="#">WebSiteName</a>
-				    </div>
+            <li id="brands_sub_brand">
+              <a class="waves-effect" style="text-decoration: none;" href="#!">Sub Brand</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </ul>
 
-				    <ul class="nav navbar-nav navbar-right">
-				      <!-- <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li> -->
-				      <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); logoutConfirmation();"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-				    </ul>
-				  </div>
-				</nav>
-				  
+  <header>
+    <ul class="dropdown-content" id="user_dropdown">
+      <li><a class="indigo-text" href="{{ route('admin.account.changemyaccountpassword') }}">Profile</a></li>
+      <li><a class="indigo-text" href="{{ route('logout') }}" onclick="event.preventDefault(); logoutConfirmation();">Logout</a></li>
+    </ul>
+
+    <nav class="indigo" role="navigation">
+      <div class="nav-wrapper">
+        <a data-activates="slide-out" class="button-collapse show-on-" href="#!"><img style="margin-top: 17px; margin-left: 5px;" src="https://res.cloudinary.com/dacg0wegv/image/upload/t_media_lib_thumb/v1463989873/smaller-main-logo_3_bm40iv.gif" /></a>
+
+        <ul class="right hide-on-med-and-down">
+          <li>
+            <a class='right dropdown-button' href='' data-activates='user_dropdown'><i class=' material-icons'>account_circle</i></a>
+          </li>
+        </ul>
+
+        <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+      </div>
+    </nav>
+
+    <nav>
+      <div class="nav-wrapper indigo darken-2">
+        <a style="margin-left: 20px;" class="breadcrumb" href="#!">Admin</a>
+        <a class="breadcrumb" href="#!">Index</a>
+
+        <div style="margin-right: 20px;" id="timestamp" class="right"></div>
+      </div>
+    </nav>
+  </header>
+
+                  <main style="height: 577px;">
+                      <h1>Main</h1>
+                  </main>
+
+  <footer class="indigo page-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col s12">
+          <h5 class="white-text">Icon Credits</h5>
+          <ul id='credits'>
+            <li>
+              Gif Logo made using <a href="https://formtypemaker.appspot.com/" title="Form Type Maker">Form Type Maker</a> from <a href="https://github.com/romannurik/FORMTypeMaker" title="romannurik">romannurik</a>
+            </li>
+            <li>
+              Icons made by <a href="https://material.io/icons/">Google</a>, available under <a href="https://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License Version 2.0</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
+    <div class="footer-copyright">
+      <div class="container">
+         <span>Made By <a style='font-weight: bold;' href="https://github.com/piedcipher" target="_blank">Tirth Patel</a></span>
+      </div>
+    </div>
+  </footer>
+</body>
 
-        <div class="top-bar">
-            <h1>Welcome to Admin DashBoard !!!!!!! </h1>
-        </div>
-        <div class="main-content">
-                    <a href="#">
-                        <img src="https://btec.fpt.edu.vn/wp-content/uploads/2022/07/LogoBTEC-1536x1268.png" alt="Logo">
-                    </a>
-        </div>
+</html>
+  <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js'></script>
+      <script id="rendered-js" >
+$('.button-collapse').sideNav();
+
+$('.collapsible').collapsible();
+
+$('select').material_select();
+//# sourceURL=pen.js
 
 
+    </script>
 
-    <footer>
-        <p style="margin-right: 240px;">© 2023 Your Website. All rights reserved.</p>
-    </footer>
-
-    <script>
+<script>
     function logoutConfirmation() {
         if (confirm('Are you sure you want to log out?')) {
             window.location.href = "{{ route('logout') }}";
         }
     }
 </script>
-
 </body>
+
 </html>
