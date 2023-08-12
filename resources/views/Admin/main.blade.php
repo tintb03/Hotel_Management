440d4f02ce17f6a4475880b76d3fe90bbe94acbd
Chumeoden <117746775+Chumeoden@users.noreply.github.com>
2023-08-12 10:34:43 +0700

    update delete footer
    update

--- resources/views/Admin/main.blade.php
+++ resources/views/Admin/main.blade.php
@@ -36,10 +36,7 @@ body {
   font-weight: bold;
 }
 
-.footer-copyright .container,
-.footer-copyright .container a {
-  color: #BCC2E2;
-}
+
 
 .fab-tip {
   position: fixed;
@@ -75,7 +72,7 @@ body {
         <img style="margin-top: 5%; border-radius: 50%;" width="100" height="100" src="https://play-lh.googleusercontent.com/vtF2gcADW6O7qnzipftCyGOyaB4pb12bjl4sMBcZp3KOOdf8DdHUJDVx0JeNeuT7nh3A"/>
 
           <p style="margin-top: -13%;">
-            Tirth Patel
+            Booking
           </p>
         </div>
       </div>
@@ -174,29 +171,6 @@ body {
                   <main style="height: 577px;">
                       <h1>Main</h1>
                   </main>
-
-  <footer class="indigo page-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col s12">
-          <h5 class="white-text">Icon Credits</h5>
-          <ul id='credits'>
-            <li>
-              Gif Logo made using <a href="https://formtypemaker.appspot.com/" title="Form Type Maker">Form Type Maker</a> from <a href="https://github.com/romannurik/FORMTypeMaker" title="romannurik">romannurik</a>
-            </li>
-            <li>
-              Icons made by <a href="https://material.io/icons/">Google</a>, available under <a href="https://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License Version 2.0</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
-         <span>Made By <a style='font-weight: bold;' href="https://github.com/piedcipher" target="_blank">Tirth Patel</a></span>
-      </div>
-    </div>
-  </footer>
 </body>
 
 </html>
