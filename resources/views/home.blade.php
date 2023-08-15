5c2aaf3a323c51b36cd7b894cd5db36042ae0f46
tintb03 <128471718+tintb03@users.noreply.github.com>
2023-08-15 12:01:09 +0700

    fornt end homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,96 +1,44 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" >
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Login and Register</title>
-  <!-- Thêm CSS của Bootstrap -->
+  <meta charset="UTF-8">
+
+    <meta name="apple-mobile-web-app-title" content="CodePen">
+
+  <title>HomePage Booking</title>
+  <link rel="stylesheet" href="{{ asset('css/mainuser.css') }}">
+  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <style>
-    /* Tùy chỉnh CSS cho modal */
-    .modal-dialog {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 100vh;
-    }
 
-    /* Tùy chỉnh màu chữ trong form */
-    label {
-      color: #333;
-    }
 
-    /* Tùy chỉnh màu chữ trong modal header */
-    .modal-header {
-      background-color: #007bff;
-      color: white;
-    }
-
-    /* Tùy chỉnh màu nút đăng nhập và đăng ký */
-    .btn-primary {
-      background-color: #007bff;
-      border-color: #007bff;
-    }
-
-    .btn-primary:hover {
-      background-color: #0056b3;
-      border-color: #0056b3;
-    }
-
-    .btn-success {
-      background-color: #28a745;
-      border-color: #28a745;
-    }
-
-    .btn-success:hover {
-      background-color: #1e7e34;
-      border-color: #1e7e34;
-    }
-
-    /* Đặt nút đăng nhập và đăng ký sang phải */
-    .login-register {
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      gap: 10px;
-      
-    }
-
-    /* Đặt logo sang trái */
-    .logo {
-      margin-right: auto;
-    }
-
-    /* Kiểu chữ hoa mỹ cho chữ Booking */
-    .booking-text {
-      font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-      font-size: 24px;
-      margin-right: 10px;
-    }
-
-    /* Dropdown styles */
-    .dropdown-menu {
-      min-width: auto;
-      transform: translate(0, 0);
-    }
-
-    .dropdown-menu a {
-      display: block;
-      padding: 8px 12px;
-    }
-  </style>
 </head>
-<body>
-<div class="login-register">
-  <div class="logo">
-    <a href="/" class="booking-text">Booking</a>
+
+<body translate="no">
+  <div class="menu-btn">
+    <i class="fas fa-bars fa-2x"></i>
   </div>
+
+  <div class="container">
+    <!-- Nav -->
+        <nav class="main-nav">
+        <img src="https://th.bing.com/th/id/OIP.TYLb_7YmvrZf6UqddYDT5wHaEH?pid=ImgDet&rs=1" alt="Booking" class="logo">
+
+        <ul class="main-menu">
+        <li><a href="#">Hotels</a></li>
+        <li><a href="#">Flights</a></li>
+        <li><a href="#">Cars</a></li>
+        <li><a href="#">Deals</a></li>
+        <li><a href="#">Support</a></li>
+        </ul>
+
+
+
+        <div class="login-register">
   <!-- Hiển thị tên người dùng và liên kết đăng xuất khi đã đăng nhập -->
   @if(Auth::check())
     <div class="dropdown">
-      <a class="dropdown-toggle" href="#" role="button" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="
-    padding-right: 34px;
-">
+      <a class="dropdown-toggle" href="#" role="button" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="padding-right: 34px;">
         {{ Auth::user()->name }}
       </a>
       <div class="dropdown-menu" aria-labelledby="userDropdown">
@@ -105,6 +53,124 @@
   @endif
 </div>
 
+
+
+        <ul class="right-menu">
+        <li>
+            <a href="#">
+            <i class="fas fa-search"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+            <i class="fas fa-shopping-cart"></i>
+            </a>
+        </li>
+        </ul>
+    </nav>
+
+
+
+
+    <!-- Showcase -->
+    <header class="showcase">
+                <!-- <h2>Surface Deals</h2>
+                <p>
+                    Select Surfaces are on sale now - save while supplies last
+                </p>
+                <a href="#" class="btn">
+                    Shop Now <i class="fas fa-chevron-right"></i>
+                </a> -->
+    </header>
+
+    <!-- Home cards 1 -->
+    <section class="home-cards">
+      <div>
+        <img src="https://i.ibb.co/LZPVKq9/card1.png" alt="">
+        <h3>New Surface Pro 7</h3>
+        <p>
+          See how Katie Sowers, Asst. Coach for the 49ers, uses Surface Pro 7
+                        to put her plans into play.
+        </p>
+        <a href="#">Learn More <i class="fas fa-chevron-right"></i></a>
+      </div>
+      
+    </section>
+
+    <!-- Home cards 2 -->
+            <section class="home-cards">
+                <div>
+                    <img src="https://i.ibb.co/zVqhWn2/card5.png" alt="" />
+                    <h3>Microsoft Teams</h3>
+                    <p>
+                        Unleash the power of your team.
+                    </p>
+                    <a href="#">Shop Now <i class="fas fa-chevron-right"></i></a>
+                </div>
+                <div>
+                    <img src="https://i.ibb.co/mGZcxcn/card6.jpg" alt="" />
+                    <h3>Unlock the power of learning</h3>
+                    <p>
+                        Get students future-ready with Windows 10 devices. Starting at $219.
+                    </p>
+                    <a href="#">Shop Now <i class="fas fa-chevron-right"></i></a>
+                </div>
+                <div>
+                    <img src="https://i.ibb.co/NpPvVHj/card7.png" alt="" />
+                    <h3>Windows 10 Enterprise</h3>
+                    <p>
+                        Download the free 90-day evaluation for IT professionals.
+                    </p>
+                    <a href="#">Download Now <i class="fas fa-chevron-right"></i></a>
+                </div>
+                <div>
+                    <img src="https://i.ibb.co/LkP4L5T/card8.png" alt="" />
+                    <h3>Explore Kubernetes</h3>
+                    <p>
+                        Learn how Kubernetes works and get started with cloud native app
+                        development today.
+                    </p>
+                    <a href="#">Get Started <i class="fas fa-chevron-right"></i></a>
+                </div>
+      </section>
+      
+      <!-- Follow -->
+      <section class="follow">
+        <p>Follow Booking.Com</p>
+        <a href="https://www.facebook.com/phamductin22/">
+          <img src="https://i.ibb.co/LrVMXNR/social-fb.png" alt="Facebook">
+        </a>
+        <a href="https://www.facebook.com/phamductin22/">
+          <img src="https://i.ibb.co/vJvbLwm/social-twitter.png" alt="Twitter">
+        </a>
+        <a href="https://www.facebook.com/phamductin22/">
+          <img src="https://i.ibb.co/b30HMhR/social-linkedin.png" alt="Linkedin">
+        </a>
+      </section>
+    </div>
+
+
+      <!-- Footer -->
+    <footer class="footer">
+        <div class="footer-inner">
+            <div><i class="fas fa-globe fa-2x"></i> English (United States)</div>
+            <ul>
+            <li><a href="#">Sitemap</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms of Use</a></li>
+            <li><a href="#">Trademarks</a></li>
+            <li><a href="#">Safety & Eco</a></li>
+            <li><a href="#">About Our Ads</a></li>
+            <li><a href="#">&copy; Booking 2023</a></li>
+            </ul>
+        </div>
+    </footer>
+
+
+
+
+    
 <!-- Modal đăng nhập -->
 <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
@@ -177,9 +243,14 @@
   </div>
 </div>
 
+
+
+
+  
 <!-- Thêm JavaScript của Bootstrap -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
